db1cc685bc42b59ab37452f58df6cf16beac804e
Leegijun <dlrlwms11@gmail.com>
2026-08-25 21:26:20 +0900

    미들웨어설정 (프론트 요청 연결)

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 from database import engine, Base
 from model.ingredients import Ingredients
 from router.ingredients import router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 @app.get("/")
 def health():
     return {"msg":"health check"}
